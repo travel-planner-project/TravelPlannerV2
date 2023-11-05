49c3249f7907df790c3075ba21ad4fe8b46840e2
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-05 23:28:03 +0900

    Feat: 유저 엔티티 생성 (#4)

---
+++ src/main/java/com/travelplanner/v2/domain/user/domain/User.java
@@ -0,0 +1,38 @@
+package com.travelplanner.v2.domain.user.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import java.io.Serializable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+@AllArgsConstructor
+public class User implements Serializable { // 레디스에 유저정보를 캐싱을 위해 구현 하였습니다.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userNickname;
+
+    @Email
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String provider;
+}
