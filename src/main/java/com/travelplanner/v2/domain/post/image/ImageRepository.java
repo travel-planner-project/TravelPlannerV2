ada8571000e9273b46cba6e322005ada2674928d
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-06 16:45:16 +0900

    Rename: 필요없는 import 삭제 (#11)

--- src/main/java/com/travelplanner/v2/domain/post/image/ImageRepository.java
+++ src/main/java/com/travelplanner/v2/domain/post/image/ImageRepository.java
@@ -1,6 +1,5 @@
 package com.travelplanner.v2.domain.post.image;
 
-import com.travelplanner.v2.domain.post.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
