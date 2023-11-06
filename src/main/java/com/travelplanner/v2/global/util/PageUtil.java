fa4fcb6ac4acba826031504c0a8be8e3e1b99b3a
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-06 15:32:42 +0900

    Feat: 페이지 유틸 생성 (#9)

---
+++ src/main/java/com/travelplanner/v2/global/util/PageUtil.java
@@ -0,0 +1,21 @@
+package com.travelplanner.v2.global.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PageUtil<T> {
+    private List<T> content;
+    private int currentPageNumber;
+    private int totalPageNumber;
+
+    public PageUtil(List<T> content, Pageable pageable, int totalPageNumber) {
+        this.content = content;
+        this.currentPageNumber = pageable.getPageNumber();
+        this.totalPageNumber = totalPageNumber;
+    }
+}
