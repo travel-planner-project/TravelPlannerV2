bc7e0d56195faacc4484c5b3e914c715b5b2fd43
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-09 23:19:16 +0900

    Design: 컨테이너 생성 (#16)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Container from './layouts/Container.vue';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+</script>
 
-<template></template>
+<template>
+  <Container />
+</template>
 
-<style scoped></style>
+<style scoped lang="scss"></style>
---
+++ frontend/src/layouts/Container.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Navbar from './Navbar.vue';
+import Content from './Content.vue';
+</script>
+
+<template>
+  <div class="container">
+    <Navbar />
+    <Content />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'layout';
+</style>
