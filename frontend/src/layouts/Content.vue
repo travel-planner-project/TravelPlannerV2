01c2edfa17741f2b0bad3aea9daa2371d14c49d0
시은 <wldsmtldsm65@gmail.com>
2023-11-10 14:30:50 +0900

    [FE] Design: 공통 레이아웃 생성 (#17)
    * Setting: assets 추가 / 부트스트랩 재설정 (#1)
    
    * Design: 컨테이너 생성 (#16)
    
    * Design: 네비게이션 바 생성 (#16)
    
    * Design: 일부 디자인 수정 (#16)
    
    * Design: 일부 디자인 수정 (#16)
    
    * Setting: assets 추가 (#1)
    
    * Design: 디자인 일부 변경 / 컨텐츠 생성 (#16)
    
    * Design: 컨텐트 뷰 추가 (#16)

---
+++ frontend/src/layouts/Content.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="content-container"></div>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/styles/common/container.scss';
+@import '../assets/styles/colors/_light.scss';
+
+.content-container {
+  @include container(column, center, center, 100%, 100%);
+  background-color: transparent;
+}
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,19 @@
 import { createApp } from 'vue';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+import { BootstrapVue3 } from 'bootstrap-vue-3';
 import { VueQueryPlugin } from 'vue-query';
 import { PiniaVuePlugin } from 'pinia';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 
-createApp(App).use(VueQueryPlugin).use(PiniaVuePlugin).mount('#app');
+library.add(faUserSecret);
+
+createApp(App)
+  .use(VueQueryPlugin)
+  .use(BootstrapVue3)
+  .use(PiniaVuePlugin)
+  .component('FontAwesomeIcon', FontAwesomeIcon)
+  .mount('#app');
