b03eb2a28226dbdc6b0e651b15f8f64657b653a6
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-11 17:47:10 +0900

    Docs: 유저 정보 스웨거 갱신 (#4)

--- src/main/java/com/travelplanner/v2/domain/user/cache/UserDTO.java
+++ src/main/java/com/travelplanner/v2/domain/user/cache/UserDTO.java
@@ -14,9 +14,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+    @Schema(description = "유저 인덱스", example = "1")
     private String userId;
+    @Schema(description = "유저 이메일", example = "test1@test.com")
     private String email;
+    @Schema(description = "유저 닉네임", example = "시은")
     private String userNickname;
+    @Schema(description = "provider", example = "google")
     private String provider;
+    @Schema(description = "유저 프로필 이미지")
     private String profileImageUrl;
 }
