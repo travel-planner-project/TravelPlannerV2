fca020ae50abaf26e90f4b8290a744b9b83637a3
sieunnnn <wldsmtldsm65@gmail.com>
2023-11-11 14:51:22 +0900

    Setting: url 변경 (#1)

--- frontend/src/api/interceptor/request-interceptor.ts
+++ frontend/src/api/interceptor/request-interceptor.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://dev.travel-planner.xyz/api',
 });
 
 axiosInstance.interceptors.request.use(
